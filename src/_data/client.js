d20f66196e105dce1e25cea3ca86972b298731a8
toka1300 <caseytokarchuk@gmail.com>
2024-09-22 15:02:18 -0400

    updated to Canadian format

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,16 +1,16 @@
 module.exports = {
-    "name": "Code Stitch Web Designs",
-    "email": "help@codestitch.app",
-    "phoneForTel": "555-779-4407",
-    "phoneFormatted": "(555) 779-4407",
+    "name": "Equanimous Edge Web Designs",
+    "email": "caseytokarchuk@gmail.com",
+    "phoneForTel": "613-857-9828",
+    "phoneFormatted": "(613) 857-9828",
     "address": {
-        "lineOne": "First Address Line",
-        "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
-        "zip": "80206",
-        "country": "US",
-        "mapLink": "https://goo.gl/maps/UAQn4vuGDiwv7DV39"
+        "lineOne": "123 Fake Street",
+        "lineTwo": "What is this line for?",
+        "city": "Ottawa",
+        "state": "ON",
+        "postalCode": "K0A1X0",
+        "country": "Canada",
+        "mapLink": "https://maps.app.goo.gl/5y6iGEJqFwFWFt9i9"
     },
     "socials": {
         "facebook": "https://www.facebook.com/",
